11bd51e5052f8f2b109411dcd470ff34214fec68
Adan Lodhi <adankhanlodhi1432@gmail.com>
2023-08-26 22:19:57 +0500

    code update for checkout button

--- ecommerce-app/client/src/components/MainNav.jsx
+++ ecommerce-app/client/src/components/MainNav.jsx
@@ -131,13 +131,6 @@ const MainNav = () => {
                             data-te-dropdown-item-ref
                           >
                             Dashboard
-                            to={`/dashboard/${auth?.user?.role === 1 ? `admin` : `user`
-                              }`}
-                            className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                            data-te-dropdown-item-ref
-                          
-                            {" "}
-                            Dashboard{" "}
                           </NavLink>
                         </li>
                         <li>
